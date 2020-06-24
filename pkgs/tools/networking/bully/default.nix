40bae86548931b9f99b20febfe2fe35d39e209fe
Peter Hoeg <peter@hoeg.com>
2020-06-24 08:49:07 +0800

    bully: add documentation

--- pkgs/tools/networking/bully/default.nix
+++ pkgs/tools/networking/bully/default.nix
@@ -1,33 +1,32 @@
 { stdenv, fetchFromGitHub, openssl, libpcap }:
 
 stdenv.mkDerivation rec {
-
   pname = "bully";
   version = "1.1";
 
   src = fetchFromGitHub {
-    sha256 = "1qvbbf72ryd85bp4v62fk93ag2sn25rj7kipgagbv22hnq8yl3zd";
-    rev = version;
-    repo = "bully";
     owner = "aanarchyy";
+    repo = "bully";
+    rev = version;
+    sha256 = "1qvbbf72ryd85bp4v62fk93ag2sn25rj7kipgagbv22hnq8yl3zd";
   };
+
   buildInputs = [ openssl libpcap ];
 
-  buildPhase = ''
-    cd src
-    make
-  '';
+  enableParallelBuilding = true;
+
+  sourceRoot = "./source/src";
 
   installPhase = ''
-    mkdir -p $out/bin
-    mv bully $out/bin
+    install -Dm555 -t $out/bin bully
+    install -Dm444 -t $out/share/doc/${pname} ../*.md
   '';
 
   meta = with stdenv.lib; {
     description = "Retrieve WPA/WPA2 passphrase from a WPS enabled access point";
     homepage = "https://github.com/aanarchyy/bully";
-    maintainers = with maintainers; [ edwtjo ];
     license = licenses.gpl3;
+    maintainers = with maintainers; [ edwtjo ];
     platforms = platforms.linux;
   };
 }
