2a16a770ee8b3ffc1b9dfe099cbd2d548212e83e
Siva Mahadevan <me@svmhdvn.name>
2020-06-30 11:59:49 -0400

    attoparsec: -dontCheck

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -781,8 +781,6 @@ self: super: {
 
   # Needs QuickCheck <2.10, which we don't have.
   edit-distance = doJailbreak super.edit-distance;
-  blaze-html = doJailbreak super.blaze-html;
-  attoparsec = dontCheck super.attoparsec;      # 1 out of 67 tests fails
   int-cast = doJailbreak super.int-cast;
   nix-derivation = doJailbreak super.nix-derivation;
 
