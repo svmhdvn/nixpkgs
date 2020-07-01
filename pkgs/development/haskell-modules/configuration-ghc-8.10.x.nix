11bbd66bf1f9b4534a427f76a6f9ec430bdf8996
Siva Mahadevan <me@svmhdvn.name>
2020-06-30 20:02:57 -0400

    ed25519: -doJailbreak

--- pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
@@ -61,7 +61,6 @@ self: super: {
   haddock-library = self.haddock-library_1_9_0;
 
   # Jailbreak to fix the build.
-  ed25519 = doJailbreak super.ed25519;
   email-validate = doJailbreak super.email-validate;  # https://github.com/Porges/email-validate-hs/issues/51
   feed = doJailbreak super.feed;  # https://github.com/bergmark/feed/issues/48
   hashable = doJailbreak super.hashable;
